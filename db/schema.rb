7c98f19287e3ca651050d24d54ab8999ea470cff
bchrkuri <bchrkuri@memphis.edu>
2025-03-17 17:17:32 -0500

    Create dog-resource scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_03_195555) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_17_201635) do
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
+    t.date "date_of_birth"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "people", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
